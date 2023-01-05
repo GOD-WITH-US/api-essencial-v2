1f48bb22eb4c238b6e14f0c1589b0f0777b4a0c4
unknown <e.cronenberger@gmail.com>
2023-01-05 01:06:42 +0100

    login & logout + token generate ok

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,19 +3,56 @@
 // Load the UserModel module
 const UserModel = require("../models/user.model");
 
-// Define the signUp function for handling user sign up
+const jwt = require("jsonwebtoken");
+
+// The auth token will be valid for 3 days before expiry
+const maxAge = 3 * 24 * 60 * 60 * 1000;
+
+// Function to create a token from the user's ID
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, {
+    expiresIn: maxAge,
+  });
+};
+
+// User signup function
 module.exports.signUp = async (req, res) => {
-  // Destructure the pseudo, email, and password from the request body
+  // Get the pseudo, email, and password from the request
   const { pseudo, email, password } = req.body;
 
   try {
-    // Create a new user with the provided pseudo, email, and password
+    // Create the user using the UserModel
     const user = await UserModel.create({ pseudo, email, password });
-
-    // Respond with a status of 201 (Created) and the ID of the newly created user for verification
+    // Send the user's ID in the response to the request
     res.status(201).json({ user: user._id });
   } catch (err) {
-    // If an error occurs, send a response with a status of 200 (OK) and the error message
-    res.status(200).send({ err });
+    // Handle errors in user creation
+    res.status(400).send({ errors: err });
   }
+};
+
+// User signin function
+module.exports.signIn = async (req, res) => {
+  // Get the email and password from the request
+  const { email, password } = req.body;
+
+  try {
+    // Sign in the user using the UserModel
+    const user = await UserModel.login(email, password);
+    // Create a token from the user's ID
+    const token = createToken(user._id);
+    // Send the token in a cookie with a maxAge lifespan
+    res.cookie("jwt", token, { httpOnly: true, maxAge });
+    // Send the user's ID in the response to the request
+    res.status(200).json({ user: user._id });
+  } catch (err) {
+    // Handle errors in user sign in
+    res.status(401).json({ errors: err });
+  }
+};
+
+// User logout function
+module.exports.logout = (req, res) => {
+  res.cookie("jwt", "", { maxAge: 1 });
+  res.redirect("/");
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,25 +4,30 @@ const router = require("express").Router();
 //loading autController
 const authController = require("../controllers/auth.controller");
 
-
-
 //loading userController
 const userController = require("../controllers/user.controller");
 
-
 // route for registry
 router.post("/register", authController.signUp);
-//route for get all user 
+// route for login
+router.post("/login", authController.signIn);
+// route for logout
+router.get("/logout", authController.logout);
+
+//route for get all user
 router.get("/", userController.getAllUsers);
 //route for one user info
 router.get("/:id", userController.userInfo);
 //route for update user
 router.put("/:id", userController.updateUser);
 //route for delete one user
-router.delete('/:id', userController.deleteOne); 
+router.delete("/:id", userController.deleteOne);
+
 //route for follow
 router.patch("/follow/:id", userController.follow);
 //route for unfollow
 router.patch("/unfollow/:id", userController.unfollow);
+
+
 //on rend notre module disponible dans toute l'app
 module.exports = router;
